29219b7fc3f87603bff144ca1e196b9939bbcb4e
CristopherMales <crissmales67@gmail.com>
2022-02-07 00:26:45 -0500

    Tercer commit Internacionalizacion

---
+++ MiPaint_fr.ts
@@ -0,0 +1,97 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>Principal</name>
+    <message>
+        <location filename="principal.ui" line="17"/>
+        <source>Principal</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="31"/>
+        <source>Archivo</source>
+        <translation>Archiver</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="40"/>
+        <source>Estilo</source>
+        <translation>Style</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="47"/>
+        <source>Forma</source>
+        <translation>Forme</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="61"/>
+        <source>Nuevo</source>
+        <translation>Nouveau</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="64"/>
+        <source>Ctrl+N</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="69"/>
+        <source>Guardar</source>
+        <translation>Garder</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="72"/>
+        <source>Ctrl+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="77"/>
+        <source>Salir</source>
+        <translation>Sortir</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="80"/>
+        <source>Ctrl+Q</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="85"/>
+        <source>Color</source>
+        <translation>Couleur</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="88"/>
+        <source>Ctrl+Shift+C</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="93"/>
+        <source>Ancho</source>
+        <translation>Large</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="96"/>
+        <source>Ctrl+Shift+A</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="101"/>
+        <source>Lineas</source>
+        <translation>Lignes</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="106"/>
+        <source>Libre</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="111"/>
+        <source>Rectánculos</source>
+        <translation>Rectangles</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="116"/>
+        <source>Circunferencias</source>
+        <translation>Circonférences</translation>
+    </message>
+</context>
+</TS>
--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
 #include "principal.h"
 
 #include <QApplication>
+#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QTranslator traducion;
+    QString idiomaSistema = QLocale::system().nativeLanguageName().toLower();
+
+        if (idiomaSistema == "english"){
+            traducion.load(":/MiPaint_en.qm");
+        }
+        //Français
+        if (idiomaSistema == "français"){
+            traducion.load(":/MiPaint_fr.qm");
+        }
+
+        if (idiomaSistema != "español"){
+            a.installTranslator(&traducion);
+        }
+
     Principal w;
     w.show();
     return a.exec();
